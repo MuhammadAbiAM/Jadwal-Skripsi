afa5a7ef5689f0452dfb563fa28b834409f169e0
MuhammadAbiAM <168069395+MuhammadAbiAM@users.noreply.github.com>
2025-03-04 13:45:45 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/api/buku', 'Userapi::index');
+$routes->get('/api/user', 'Userapi::index');
+$routes->post('/api/user', 'Userapi::create');
+$routes->put('/api/user/(:num)', 'Userapi::update/$1');
+$routes->delete('/api/user/(:num)', 'Userapi::delete/$1');
