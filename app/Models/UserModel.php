9b1f0e1764f11e6707a1c1881a77739d7398dc3b
MuhammadAbiAM <168069395+MuhammadAbiAM@users.noreply.github.com>
2025-03-04 13:46:21 +0700

    Update UserModel.php

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -3,13 +3,12 @@ namespace App\Models;
 use CodeIgniter\Model;
 
 class UserModel extends Model {
-    protected $table = 'buku';
-    protected $primaryKey = 'id';
-    protected $allowFields = ['loker_buku', 'no_rek', 'no_laci', 'no_boks', 'judul_buku', 'nama_pengarang', 'tahun_terbit', 'penerima', 'penerbit', 'status', 'keterangan'];
-
-    public function getAllUser()
-    {
-        return $this->findAll();
-    }
+    protected $table = 'user';
+    protected $primaryKey = 'id_user';
+    protected $allowedFields = [
+        'username', 
+        'password', 
+        'status'
+    ];
 }
 ?>
